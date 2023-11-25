b8a3efc1430fe6de8342f2ef6edd30586b5bfaf1
Marceau Malégeant <soetchdev@gmail.com>
2023-11-25 12:46:35 +0100

    db

--- components/DailyKanji.js
+++ components/DailyKanji.js
@@ -1,7 +1,48 @@
-export default function DailyKanji() {
+import React, { useState } from 'react';
+
+const fs = require("fs")
+
+// Read the JSON file
+const rawData = fs.readFileSync('jlptn5.json');
+const jlptn5Data = JSON.parse(rawData);
+
+const getRandomUnusedId = () => {
+    let randomId;
+
+    do {
+        randomId = Math.floor(Math.random() * jlptn5Data.jlptn5.length) + 1;
+    } while (usedIds.has(randomId));
+
+    usedIds.add(randomId);
+    return randomId;
+};
+
+const getRandomKanjiById = (id) => {
+    const kanjiEntry = jlptn5Data.jlptn5.find(entry => entry.id === id);
+
+    if (kanjiEntry) {
+        return kanjiEntry.kanji;
+    } else {
+        return "Kanji not found";
+    }
+};
+
+const KanjiDisplay = () => {
+    const [usedIds, setUsedIds] = useState(new Set());
+    const [currentId, setCurrentId] = useState(getRandomUnusedId());
+
+    const handleClickNewKanji = () => {
+        const newId = getRandomUnusedId();
+        setCurrentId(newId);
+    };
+
     return (
-        <>
-        
-        </>
-    )
-}
+        <div>
+            <h1>Random Kanji Display</h1>
+            <p>Kanji: {getRandomKanjiById(currentId)}</p>
+            <button onClick={handleClickNewKanji}>New Random Kanji</button>
+        </div>
+  );
+};
+
+export default DailyKanji;
